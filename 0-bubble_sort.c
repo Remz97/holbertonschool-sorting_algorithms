2c475256b8b07b1c238807313273a0e2f7128de6
FloriMecaj <florimecaj31@gmail.com>
2024-01-11 09:37:13 +0000

    bubble sort fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,6 +11,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, temp;
 
+    if (array == NULL || size == 0)
+        return;
+
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - 1 - i; j++)
